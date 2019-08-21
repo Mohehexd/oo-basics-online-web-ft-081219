05f41b83bfe9f1a406c601e5d6085782807d7429
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-21 13:39:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -31,8 +31,12 @@ class Book
   #   @genre 
   # end 
     
-  attr_accessor :author, :page_count, :genre, :title 
+  attr_accessor :author, :page_count, :genre
   attr_reader :title
+  
+  def initialize(title)
+    @title = title 
+  end 
     
    def turn_page
      puts "Flipping the page...wow, you read fast!"
