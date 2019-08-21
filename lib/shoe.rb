90a714533fcf38cedcc86bc3fca16b27e6377305
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-21 14:20:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,7 @@ class Shoe
   def initialize(brand)
     @brand = brand 
   end
-    
-  # def cobble(cobble=string)
-  #   @cobble = cobble 
-  # end
-  
-  # def cobble
-  #   @cobble
-  # end 
-    
+
   def cobble
     puts "Your shoe is as good as new!"
   end
