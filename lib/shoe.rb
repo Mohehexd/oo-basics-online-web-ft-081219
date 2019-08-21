e019044b93719b542a8565afa32aeaec02910ae4
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-21 14:18:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :cobble, :condition
-  attr_reader :brand, :cobble
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand 
